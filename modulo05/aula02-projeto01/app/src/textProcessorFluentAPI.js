9b2e4c77f42691718719c187d5bef9759b32089b
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-06-01 12:27:16 -0300

    study: modulo05-aula02-projeto01

---
+++ modulo05/aula02-projeto01/app/src/textProcessorFluentAPI.js
@@ -0,0 +1,36 @@
+// o objetivo do Fluent API é executar métodos em sequência
+// como um pipeline, step by step
+// e no fim, chama o build. Muito similar ao padrão Builder
+// a diferença que aqui é sobre métodos encadeados,
+// o Builder sobre a construção de objetos
+class TextProcessorFluentAPI {
+  // propriedade privada!
+  #content;
+  constructor(content) {
+    this.#content = content;
+  }
+  extractPeopleData() {
+    // ?<= vai extrair os dados que virao depois desse grupo
+    // [contratada|contratante]:\s{1} vai extrair contratada ou contratante
+    // :\s{1} vai extrair os dois pontos e um espaço
+    // .*\n pega qualquer coisa até o final da linha
+    // .*? non greety, esse ? faz com que ele pare na primeira ocorrência, assim ele evita ficar em loop
+    // (?!s) negative lookahead para não pegar espaços em branco
+    // $ informar que é o final da linha
+    // g -> global, pega todas as ocorrências
+    // i -> case insensitive
+    // m -> multiline
+    const matchPerson =
+      /(?<=(?:contratada|contratante):\s{1})(?!\s)(.*\n.*?)$/gim;
+    // faz o match para encontrar a string inteira que contém os dados que precisamos
+    const onlyPerson = this.#content.match(matchPerson);
+    // console.log("onlyPerson", matchPerson.test(this.#content));
+    return onlyPerson;
+  }
+
+  build() {
+    return this.#content;
+  }
+}
+
+module.exports = TextProcessorFluentAPI;
---
+++ modulo05/aula02-projeto01/app/test/textProcessorFluentAPI.test.js
@@ -0,0 +1,24 @@
+const { describe, it } = require("mocha");
+const { expect } = require("chai");
+const TextProcessorFluentAPI = require("../src/textProcessorFluentAPI");
+const mock = require("./mock/valid");
+
+describe("TextProcessorFluentAPI test suite", () => {
+  it("#build", () => {
+    const result = new TextProcessorFluentAPI(mock).build();
+    expect(result).to.be.deep.equal(mock);
+  });
+
+  it("#extractPeopleData", () => {
+    const result = new TextProcessorFluentAPI(mock).extractPeopleData();
+    const expected = [
+      "Xuxa da Silva, brasileira, casada, CPF 235.743.420-12, residente e\n" +
+        "domiciliada a Rua dos bobos, zero, bairro Alphaville, São Paulo.",
+      "Arya Robbin, belga, casado, CPF 884.112.200-52, residente e\n" +
+        "domiciliada a Av. paulista, 1400, bairro Consolação, São Paulo.",
+      "Júlia Menezes, brasileira, solteira, CPF 297.947.800-81, residente e\n" +
+        "domiciliada a Av. dos Estados, 99, bairro Jardins, São Paulo.",
+    ];
+    expect(result).to.be.deep.equal(expected);
+  });
+});
