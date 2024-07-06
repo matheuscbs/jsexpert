8dc77ca369a8d61e567850d4ac0680a6ec7b4b0d
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-07-06 16:49:22 -0300

    study: benchmarking performance

---
+++ 12.Performance/01.benchmarking/src/entities/cart.js
@@ -0,0 +1,23 @@
+import Product from "./product.js";
+
+export default class Cart {
+  constructor({ at, products }) {
+    this.at = at;
+    this.products = this.removeUndefinedProps(products);
+    this.total = this.getCartPrice();
+  }
+
+  removeUndefinedProps(products) {
+    const productsEntities = products
+      .filter((product) => !!Reflect.ownKeys(product).length)
+      .map((product) => new Product(product));
+
+    return JSON.parse(JSON.stringify(productsEntities));
+  }
+
+  getCartPrice() {
+    return this.products
+      .map((product) => product.price)
+      .reduce((prev, price) => prev + price, 0);
+  }
+}
